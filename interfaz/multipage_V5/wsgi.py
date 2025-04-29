ebeb11fba168a25319cd2e2f54f93d3503ebc3d0
amparogabrielli <111793575+amparogabrielli@users.noreply.github.com>
2025-04-29 18:23:32 -0300

    Update wsgi.py

--- interfaz/multipage_V5/wsgi.py
+++ interfaz/multipage_V5/wsgi.py
@@ -1,7 +1,2 @@
 from app import app
-
-server = app.server  # Esto expone el servidor Flask subyacente de Dash
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run_server(host="0.0.0.0", port=port, debug=False)
+server = app.server  
