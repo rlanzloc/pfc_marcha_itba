a7a8be9d9ec3bbc153f43c278de309de698d0c89
amparogabrielli <111793575+amparogabrielli@users.noreply.github.com>
2025-04-28 22:58:39 -0300

    Update wsgi.py

--- interfaz/multipage_V5/wsgi.py
+++ interfaz/multipage_V5/wsgi.py
@@ -1,5 +1,7 @@
 from app import app
 
+server = app.server  # Esto expone el servidor Flask subyacente de Dash
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Usa el puerto que Render asigna
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run_server(host="0.0.0.0", port=port, debug=False)
